6fb9e7905d572c2f84923038c20d13f83851101a
Haynar216 <1500012913@pku.edu.cn>
2019-11-12 14:43:31 +0800

    update api to 4 and fix bug

--- app/src/main/java/systems/v/coldwallet/Wallet/Transaction.java
+++ app/src/main/java/systems/v/coldwallet/Wallet/Transaction.java
@@ -231,7 +231,7 @@ public class Transaction {
                                                  long fee, short feeScale,  BigInteger timestamp)
     {
         byte[] attachmentBytes = (description == null ? "" : description).getBytes();
-        ByteBuffer buf = ByteBuffer.allocate(KBYTE);
+        ByteBuffer buf = ByteBuffer.allocate(2048);
         buf.put(CREATE_CONTRACT);
 
 
